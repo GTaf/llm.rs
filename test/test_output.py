cf0f6827e2b991af4c84a33c813635cf7c2640c2
GTaf <3484782+GTaf@users.noreply.github.com>
2025-10-29 22:12:10 +0100

    Update test sample generation

--- test/test_output.py
+++ test/test_output.py
@@ -1,9 +1,9 @@
 import json
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPT2Model, GPT2Tokenizer
 import torch
 
 # Load model and tokenizer
-model = GPT2LMHeadModel.from_pretrained("gpt2")
+model = GPT2Model.from_pretrained("gpt2")
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
 # Input text
@@ -12,10 +12,11 @@ input_ids = tokenizer.encode(text, return_tensors="pt")
 print(input_ids[0][0])
 
 # Get embeddings (output of embedding layer)
+model.eval()
 with torch.no_grad():
     saving_dict = dict()
     # Token embeddings
-    token_embeddings = model.transformer.wte(input_ids)
+    token_embeddings = model.wte(input_ids)
     # print("Token embeddings shape:", token_embeddings.shape)
     print("Token embeddings:\n", token_embeddings[0][0])
     saving_dict["Token embeddings"] = token_embeddings[0][0].tolist()
@@ -23,7 +24,7 @@ with torch.no_grad():
     # Positional embeddings
     seq_length = input_ids.size(1)
     position_ids = torch.arange(seq_length).unsqueeze(0)
-    position_embeddings = model.transformer.wpe(position_ids)
+    position_embeddings = model.wpe(position_ids)
     # print("\nPosition embeddings shape:", position_embeddings.shape)
     print("Position embeddings:\n", position_embeddings[0][0])
     saving_dict["Position embeddings"] = position_embeddings[0][0].tolist()
@@ -32,5 +33,10 @@ with torch.no_grad():
     combined = token_embeddings + position_embeddings
     print("\nCombined embeddings:\n", combined[0][0])
     saving_dict["Combined embeddings"] = combined[0][0].tolist()
+
+    first_layer_first_norm = model.h[0].ln_1(combined)
+    print("First layer norm:\n", first_layer_first_norm[0][0])
+    saving_dict["First layer norm"] = first_layer_first_norm[0][0].tolist()
+
     with open("test/test_data.dump", "w") as f:
         json.dump(saving_dict, f)
